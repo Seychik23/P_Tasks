ff7f81e184e9179da456dacb164358620fce6082
Seychik23 <1132227119@pfur.ru>
2024-02-19 14:00:48 +0300

    Updated tasks

--- 3.py
+++ 3.py
@@ -1,14 +1,18 @@
+#Даны натуральные числа а и b, обозначающие соответственно числитель и знаменатель
+#дроби. Сократить дробь, т. е. найти такие натуральные числа р и q, не имеющие общих
+#делителей, что p/q = a/b
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
-def simplify_fraction(a, b):
+def simplifyfraction(a, b):
     common_divisor = gcd(a, b)
     return a // common_divisor, b // common_divisor
 
 a = int(input("Input a"))
 b = int(input("Input b"))
-p, q = simplify_fraction(a, b)
+p, q = simplifyfraction(a, b)
 
 print(f"The simplified fraction of {a}/{b} is {p}/{q}")
--- 6.py
+++ 6.py
@@ -1,11 +1,16 @@
-def palindrome_check(num):
+#Даны два натуральных числа. Выяснить, является ли хоть одно из них палиндромом, т. е.
+#таким числом, десятичная запись которого читается одинаково слева направо и справа на
+#лево
+
+
+def palindromecheck(num):
     num_str = str(num)
     return num_str == num_str[::-1]
 
 num1 = int(input("Введите первое натуральное число: "))
 num2 = int(input("Введите второе натуральное число: "))
 
-if palindrome_check(num1) or palindrome_check(num2):
+if palindromecheck(num1) or palindromecheck(num2):
     print("Хотя бы одно из чисел является палиндромом!")
 else:
     print("Ни одно из этих чисел не является палиндромом!")
